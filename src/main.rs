e8c19af1184a98e40c4a9b89174a0d792eda59ce
yngtodd <young.todd.mk@gmail.com>
2019-05-04 23:16:43 -0400

    handle key presses

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,38 @@ const SCREEN_HEIGHT: i32 = 80;
 const SCREEN_WIDTH: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt + Enter: toggle fullscreen
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+
+        // Exit the game
+        Key { code: Escape, .. } => return true,
+
+        // Movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {},
+    }
+
+    false
+}
+
 fn main() {
     let mut root = Root::initializer()
         .font("assets/arial10x10.png", FontLayout::Tcod)
@@ -17,11 +49,20 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS); 
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
         root.clear();
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         root.flush();
-        root.wait_for_keypress(true);
+
+        // Handle keys and exit if needed.
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
+
     }
 }
