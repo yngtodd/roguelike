ce5defbd253ca4cd516a23e08884f2fa56927889
yngtodd <young.todd.mk@gmail.com>
2019-05-05 12:52:41 -0400

    Add tiles and map, new render fn

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,37 @@ extern crate tcod;
 use tcod::colors::{self, Color};
 use tcod::console::*;
 
-const SCREEN_HEIGHT: i32 = 80;
-const SCREEN_WIDTH: i32 = 50;
+// Size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+// Size of the map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+// Maximum number of frames per second
 const LIMIT_FPS: i32 = 20;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+type Map = Vec<Vec<Tile>>;
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    blocked_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, blocked_sight: false }
+    }
+
+    pub fn wall() -> Self {
+        Tile { blocked: true, blocked_sight: true }
+    }
+}
 
 struct Object {
     x: i32,
@@ -34,6 +62,38 @@ impl Object {
         con.set_default_foreground(self.color);
         con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
     }
+}
+
+fn make_map() -> Map {
+    // Fill with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].blocked_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    // draw all objects in the list
+    for object in objects {
+        object.draw(con);
+    }
+
+    // blit the contents of "con" to the root console
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
 
 fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
@@ -79,25 +139,19 @@ fn main() {
     let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+   
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+
     let mut objects = [player, npc];
 
+    let map = make_map();  
+
     while !root.window_closed() {
+        // Clear the screen of the previous frame
         con.clear();
 
-        for object in &objects {
-            object.draw(&mut con);
-        }
-
-        blit(
-            &mut con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
+        // Render the screen
+        render_all(&mut root, &mut con, &objects, &map);
 
         root.flush();
 
