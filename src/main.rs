8acbb59504e4c1ee45a545d7bc377fde9e15461e
yngtodd <young.todd.mk@gmail.com>
2019-05-05 01:27:21 -0400

    generalize objects

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
 extern crate tcod;
 
 use tcod::console::*;
-use tcod::colors;
+use tcod::colors::{self, Color};
 
 const SCREEN_HEIGHT: i32 = 80;
 const SCREEN_WIDTH: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y:i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        // Move by a given amount
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -28,10 +57,10 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
         Key { code: Escape, .. } => return true,
 
         // Movement keys
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         _ => {},
     }
@@ -50,13 +79,17 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS); 
     let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let npc = Object::new(SCREEN_WIDTH/2-5, SCREEN_HEIGHT/2, '@', colors::YELLOW);
+    let mut objects = [player, npc];
 
     while !root.window_closed() {
-        con.set_default_foreground(colors::WHITE);
         con.clear();
-        con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
+        for object in &objects {
+            object.draw(&mut con);
+        }
+
         blit(
             &mut con, 
             (0, 0),
@@ -66,13 +99,14 @@ fn main() {
             1.0,
             1.0
         );
+
         root.flush();
 
         // Handle keys and exit if needed.
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player);
         if exit {
             break;
         }
-
     }
 }
