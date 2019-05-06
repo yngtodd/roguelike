62432803be721dcf9d61b3858f63675a68761c2d
yngtodd <young.todd.mk@gmail.com>
2019-05-06 16:49:42 -0400

    Add field of view algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 extern crate rand;
 extern crate tcod;
 
-use std::cmp;
 use rand::Rng;
+use std::cmp;
 
 use tcod::colors::{self, Color};
 use tcod::console::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 
 // Size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -22,11 +23,26 @@ const MAX_ROOMS: i32 = 30;
 // Maximum number of frames per second
 const LIMIT_FPS: i32 = 20;
 
+// Field of View
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true;
+const TORCH_RADIUS: i32 = 10;
+
 const COLOR_DARK_WALL: Color = Color { r: 38, g: 0, b: 77 };
+const COLOR_LIGHT_WALL: Color = Color {
+    r: 130,
+    g: 110,
+    b: 50,
+};
 const COLOR_DARK_GROUND: Color = Color {
     r: 51,
     g: 0,
     b: 128,
+};
+const COLOR_LIGHT_GROUND: Color = Color {
+    r: 200,
+    g: 180,
+    b: 50,
 };
 
 type Map = Vec<Vec<Tile>>;
@@ -79,8 +95,10 @@ impl Rect {
 
     pub fn intersects_with(&self, other: &Rect) -> bool {
         // Returns true if this rectangle intersects with another.
-        (self.x1 <= other.x2) && (self.x2 >= other.x1) &&
-             (self.y1 <= other.y2) && (self.y2 >= other.y1)
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
     }
 }
 
@@ -164,7 +182,7 @@ fn make_map() -> (Map, (i32, i32)) {
                     create_h_tunnel(prev_x, new_x, new_y, &mut map);
                 }
             }
-            
+
             // Append the new room to the list of rooms;
             rooms.push(new_room);
         }
@@ -194,16 +212,33 @@ fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
     }
 }
 
-fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+fn render_all(
+    root: &mut Root,
+    con: &mut Offscreen,
+    objects: &[Object],
+    map: &Map,
+    fov_map: &mut FovMap,
+    fov_recompute: bool,
+) {
+    if fov_recompute {
+        let player = &objects[0];
+        fov_map.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+    }
+
     // go through all tiles, and set their background color
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = fov_map.is_in_fov(x, y);
             let wall = map[x as usize][y as usize].block_sight;
-            if wall {
-                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-            }
+            let color = match (visible, wall) {
+                // Outside the field of view
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // Inside field of view
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+            con.set_char_background(x, y, color, BackgroundFlag::Set);
         }
     }
 
@@ -266,12 +301,36 @@ fn main() {
 
     let mut objects = [player];
 
+    let mut fov_map = FovMap::new(MAP_WIDTH, MAP_HEIGHT);
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            fov_map.set(
+                x,
+                y,
+                !map[x as usize][y as usize].block_sight,
+                !map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+
+    // Prime the FOV with a player starter postion before game loop.
+    let mut previous_player_position = (-1, -1);
+
     while !root.window_closed() {
         // Clear the screen of the previous frame
         con.clear();
 
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+
         // Render the screen
-        render_all(&mut root, &mut con, &objects, &map);
+        render_all(
+            &mut root,
+            &mut con,
+            &objects,
+            &map,
+            &mut fov_map,
+            fov_recompute,
+        );
 
         root.flush();
 
